48df22952b15919ae805e71c4092e95c61010a85
Afrin Nahar <afrinnahar@gmail.com>
2023-08-15 23:44:52 +0600

    Updated footer, nav and experience section

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -15,7 +15,7 @@ const Education = () => {
           <p className="text-sm text-designColor tracking-[4px]">2019 - 2022</p>
           <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[20rem] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full h-[60%] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Bachelor of Social Science"
             subTitle="Jatya Kabi Kazi Nazrul Islam University"
@@ -28,16 +28,16 @@ const Education = () => {
 
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2023 Feb - Present</p>
+          <p className="text-sm text-designColor tracking-[4px]">June 2023 - July 2023</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[20rem] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full h-[60%] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Full Stack 
-            Web Developer"
-            subTitle="Upwork and Fiverr"
-            result="USA"
-            des="Started freelancing to upskill myself through working for clients. Recieved challenging tasks which I really loved to do."
+            Web Developer Intern (Remote)"
+            subTitle="Metricx"
+            result="India"
+            des="Led a team of three interns in developing a complex project using Next.js, TailwindCSS, and MongoDB, completed 70% of the website and hosted on AWS. References and source code is available."
           />
         </div>
       </div>
--- src/components/resume/ResumeCard.js
+++ src/components/resume/ResumeCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ResumeCard = ({title,subTitle,result,des}) => {
   return (
-    <div className="w-full h-[17rem] group flex">
+    <div className="w-full h-full group flex">
       <div className="w-10 h-[6px] bgOpacity mt-16 relative">
         <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-black bg-opacity-60">
           <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
