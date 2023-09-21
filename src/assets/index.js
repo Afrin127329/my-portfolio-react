96822ef30352efd42051f4eb36bd1eeaf6a41f62
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-21 23:43:06 +0600

    Updated projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,12 @@ import afrin12345 from './images/contact/afrin12345.jpg';
 import contactImg from "./images/contact/contactImg.png";
 import ecommerce from "./images/projects/ecommerce.png";
 import assitant from "./images/projects/email_assistant.png";
+import finanseer1 from './images/projects/fi1.png';
+import finanseer2 from './images/projects/fi2.png';
+import fitness1 from './images/projects/fi3.png';
+import fitness2 from './images/projects/fi4.png';
+import fitness3 from './images/projects/fi5.png';
+import fitness4 from './images/projects/fi6.png';
 import fiverr from "./images/projects/fiverr_clone.png";
 import gericth from './images/projects/gericth.png';
 import gericth2 from './images/projects/gericth2.png';
@@ -16,21 +22,11 @@ import projectTwo from "./images/projects/projectTwo.jpg";
 import prompt from "./images/projects/promptopia.png";
 
 export {
-  fiverr,
-  assitant,
-  ecommerce,
-  prompt,
-  bannerImg,
-  bannerImg2,
-  projectOne,
-  projectTwo,
-  projectThree,
-  metaversus,
-  gericth,
+  afrin12345, assitant, bannerImg,
+  bannerImg2, contactImg, ecommerce, finanseer1,
+  finanseer2, fitness1, fitness2, fitness3, fitness4, fiverr, gericth,
   gericth2,
   gpt3,
-  hoobank,
-  contactImg,
-  afrin12345
+  hoobank, metaversus, projectOne, projectThree, projectTwo, prompt
 };
 
